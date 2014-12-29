e92d56dc4a1a1bdd5d4e2c6916829f9676fea0d0
JustusAdam <development@justusadam.com>
2014-12-30 00:31:10 +0100

    added directory count

--- linecounter.py
+++ linecounter.py
@@ -27,28 +27,28 @@ def count_file(file):
         return count_lines(file.readlines())
 
 
-def count(directory, recursive=True, fileendings={'.py'}):
+def count(directory, recursive=True, file_endings={'.py'}):
     directory = pathlib.Path(directory)
 
-    if not directory.is_dir() and directory.suffix in fileendings:
+    if not directory.is_dir() and directory.suffix in file_endings:
         return count_file(directory)
 
     line_count = 0
 
     for file in directory.iterdir():
         if file.is_dir() and recursive:
-            line_count += count(file, fileendings=fileendings, recursive=True)
-        elif file.suffix in fileendings:
+            line_count += count(file, fileendings=file_endings, recursive=True)
+        elif file.suffix in file_endings:
             line_count += count_file(file)
 
     return line_count
 
 
-def count_with_highest(directory, recursive=True, fileendings={'.py'}, skip=set()):
+def count_with_highest(directory, recursive=True, file_endings={'.py'}, skip=set()):
     if not isinstance(directory, pathlib.Path):
         directory = pathlib.Path(directory)
 
-    if not directory.is_dir() and directory.suffix in fileendings:
+    if not directory.is_dir() and directory.suffix in file_endings:
         a = count_file(directory)
         return a, a
 
@@ -65,23 +65,26 @@ def count_with_highest(directory, recursive=True, fileendings={'.py'}, skip=set(
 
     highest = (0, None)
 
+    dir_count = 0
+
     for file in directory.iterdir():
         if not check_skipped(file):
             continue
         elif file.is_dir() and recursive:
-            file_count, file_highest, nfilecount = count_with_highest(file, recursive, fileendings, skip)
-            line_count += file_count
-            filecount += nfilecount
+            fline_count, file_highest, sfile_count, fdir_count = count_with_highest(file, recursive, file_endings, skip)
+            line_count += fline_count
+            filecount += sfile_count
+            dir_count += 1 + fdir_count
             if file_highest[0] > highest[0]:
                 highest = file_highest
-        elif file.suffix in fileendings:
+        elif file.suffix in file_endings:
             b = count_file(file)
             filecount += 1
             line_count += b
             if b > highest[0]:
                 highest = (b, str(file))
 
-    return line_count, highest, filecount
+    return line_count, highest, filecount, dir_count
 
 
 if __name__ == '__main__':
@@ -90,7 +93,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('scanpath', type=str)
-    parser.add_argument('--fileendings', type=str, default={'.py'})
+    parser.add_argument('--fileendings', type=str, default='.py')
     parser.add_argument('--recursive', type=bool, default=True)
     parser.add_argument('--skip_dir', type=str, default=None)
 
@@ -100,8 +103,8 @@ if __name__ == '__main__':
     skip_dir = set(args.skip_dir.split(',')) if args.skip_dir else set()
 
     print('showing lines for files ending with', fileendings)
-    count, highest, filecount = count_with_highest(args.scanpath, args.recursive, fileendings, skip_dir)
-    print('accumulative count', count, 'lines in', filecount, 'files')
+    count, highest, filecount, dir_count = count_with_highest(args.scanpath, args.recursive, fileendings, skip_dir)
+    print('accumulative count', count, 'lines in', filecount, 'files in', dir_count, 'directories')
     print()
     print('most lines in single file', highest[0])
     print('found in file', highest[1])
