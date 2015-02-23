d1a8ede4cb121bf17fee7968af3e7892786a6d39
JustusAdam <development@justusadam.com>
2015-02-23 20:18:00 +0100

    added wu internet connection checking script, added directory sizing script, made pingserver executable from command line

---
+++ checkconn.py
@@ -0,0 +1,39 @@
+import subprocess
+import io
+
+
+def main():
+    # iolog = io.BytesIO()
+    for action, status, message in perform_requests():
+        print(
+            '%-40s    %-10s    %-30s ' % (action, status, message)
+        )
+
+
+def perform_requests():
+    for address, action, error_message in (
+        ('localhost', 'Computer internal networking',  'Your computer does not exist?'),
+        ('192.168.0.1', 'Router reachability', 'Perhaps it is offline'),
+        ('141.30.202.1', 'Wundtstraße switch',
+        'The house switch is not reachable, your buildings internet is offline'),
+        ('141.30.228.39', 'Primary Wundtstraße DNS','DNS server unreachable'),
+        ('141.30.228.4', 'Secondary Wundtstraße DNS', 'DNS server unreachable'),
+        ('wh2.tu-dresden.de', 'WH2 DNS resolving', 'DNS resolving is screwed'),
+        ('141.30.235.81', 'Weberplatz connection', 'Our gate to the world is closed.'),
+        ('8.8.8.8', 'Google\'s DNS', 'ALL THE INTERNET IS GONE FOREVER.'),
+        ('google.com', 'Google.com website', 'Big internet DNS resolving does not work')
+    ):
+        code = perform_request(address)
+        if code == 0:
+            yield action, 'working', ''
+        else:
+            yield action, 'error', error_message + '  code: %s' % code
+
+def perform_request(address):
+    return subprocess.call(
+        ('ping', '-c', '3', address), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        )
+
+
+if __name__ == '__main__':
+    main()
--- pingserver.py
+++ pingserver.py
@@ -1,4 +1,7 @@
 import time
+import sys
+import threading
+from datetime import datetime
 from urllib import request
 from urllib.error import URLError
 
@@ -12,3 +15,34 @@ def ping(url, timeout=30):
         except URLError as error:
             print('still waiting', error)
             time.sleep(timeout)
+
+
+def test(page, n, l=100):
+    a = []
+    for i in range(n):
+        b = datetime.now()
+        for s in range(l):
+            request.urlopen(page)
+        a.append(datetime.now() - b)
+        print(a[-1])
+    return sum(q.total_seconds() for q in a) / len(a)
+
+
+def main():
+    if len(sys.argv) == 2:
+        url, timeout = sys.argv[1], 30
+    elif len(sys.argv) == 3:
+        url, timeout = sys.argv[1:3]
+        timeout = int(timeout)
+    else:
+        print(len(sys.argv))
+        print(sys.argv)
+        raise ValueError('Wrong number of command line arguments')
+
+    t1 = threading.Thread(target=ping, args=(url, timeout))
+
+    t1.start()
+
+
+if __name__ == '__main__':
+    main()
