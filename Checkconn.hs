d2751c61fdebda6b070230829fc5a936b2d8adbb
Justus Adam <dev@justus.science>
2016-02-24 15:20:50 +0100

    Made checkconn.hs better

--- Checkconn.hs
+++ Checkconn.hs
@@ -4,29 +4,52 @@ module Main where
 
 import System.Process
 import System.Exit
+import Data.Traversable
 import Data.Foldable
+import Control.Lens
+import Control.Concurrent.Async
+import Text.Printf
 
 
 nodes =
-  [ ("localhost", "Computer internal networking",  "Your computer does not exist?")
-  , ("192.168.0.1", "Router reachability", "Perhaps it is offline")
-  , ("141.30.202.1", "Wundtstraße switch",
-    "The house switch is not reachable, your buildings internet is offline")
-  , ("141.30.228.39", "Primary Wundtstraße DNS","DNS server unreachable")
-  , ("141.30.228.4", "Secondary Wundtstraße DNS", "DNS server unreachable")
-  , ("wh2.tu-dresden.de", "WH2 DNS resolving", "DNS resolving is screwed")
-  , ("141.30.235.81", "Weberplatz connection", "Our gate to the world is closed.")
-  , ("8.8.8.8", "Google\"s DNS", "ALL THE INTERNET IS GONE FOREVER.")
-  , ("google.com", "Google.com website", "Big internet DNS resolving does not work")
-  ]
+    [ ("localhost", "Computer internal networking",  "Your computer does not exist.")
+    , ("192.168.0.1", "Router reachability", "The router may be offline.")
+    , ("141.30.202.1", "Wundtstraße switch",
+    "Your buildings internet may be offline.")
+    , ("141.30.228.39", "Primary Wundtstraße DNS","DNS server down.")
+    , ("141.30.228.4", "Secondary Wundtstraße DNS", "Second DNS server down.")
+    , ("wh2.tu-dresden.de", "WH2 DNS resolving", "DNS resolving is down.")
+    , ("141.30.235.81", "Weberplatz connection", "The Research network is down.")
+    , ("8.8.8.8", "Google\"s DNS", "ALL THE INTERNET IS GONE FOREVER.")
+    , ("google.com", "Google.com website", "Big internet DNS resolving does not work")
+    ]
 
 
-ping url =
-  (<$> readCreateProcessWithExitCode "ping" [url]) $ \case
-    (ExitSuccess, _, _) -> True
-    _ -> False
+ping url = do
+    resp <- readCreateProcessWithExitCode (proc "ping" ["-c", "3", url]) ""
+    case resp of
+        (ExitSuccess, _, _) -> return True
+        _ -> return False
 
 
-main =
-  for_ nodes $ \(url, type', message) -> do
-    reachable <- ping url
+tableFormat = "%-20s | %-35s | %-s\n"
+
+
+main = do
+    connections <- mapM (async . ping . (^. _1)) nodes
+
+    printf tableFormat "node" "purpose" "status"
+
+    putStrLn $ replicate 80 '-'
+
+    results <- for (zip connections nodes) $ \(conn, (target, type', message)) -> do
+        reached <- wait conn
+        printf tableFormat target type' $ if reached then "✅" else "❌"
+        return (reached, target, message)
+
+    putStrLn "\n\n"
+
+    case find (not . (^. _1)) results of
+        Nothing -> putStrLn "All checks positive! No problems found"
+        Just (_, target, message) ->
+            printf "Failed when trying to reach %s, likely problem: %s" target message
